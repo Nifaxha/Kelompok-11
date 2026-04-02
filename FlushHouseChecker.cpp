ad2385b026fb626b7648cafe668030447f54445a
Nifaxha <harizriza30@gmail.com>
2026-04-03 01:11:05 +0700

    Update FlushHouseChecker

--- FlushHouseChecker.cpp
+++ FlushHouseChecker.cpp
@@ -1,7 +1,18 @@
-#pragma once
-#include "PokerHandChecker.h"
+#include <iostream>
+#include "FlushHouseChecker.h"
 
-class FiveOfAKindChecker : public PokerHandChecker{
-public:
-    HandRank check(const Hand& hand) override;
-};
+bool isFlushHouse(const Hand& hand) {
+    return hand.value == 11; // contoh dummy
+}
+
+HandRank FlushHouseChecker::check(const Hand& hand) {
+    if (isFlushHouse(hand)) {
+        std::cout << "Detected FLUSH HOUSE\n";
+        return HandRank::FLUSH_HOUSE;
+    }
+
+    if (nextChecker)
+        return nextChecker->check(hand);
+
+    return HandRank::HIGH_CARD;
+}
