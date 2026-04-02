7ad4e7e1b509304b0a318d694fc0756765704b1a
Nifaxha <harizriza30@gmail.com>
2026-04-03 00:51:13 +0700

    Update FiveOfAKindChecker

--- FiveOfAKindChecker.cpp
+++ FiveOfAKindChecker.cpp
@@ -1,7 +1,18 @@
-#pragma once
-#include "PokerHandChecker.h"
+#include <iostream>
+#include "FiveOfAKindChecker.h"
 
-class RoyalFlushChecker : public PokerHandChecker{
-public:
-    HandRank check(const Hand& hand) override;
-};
+bool isFiveOfAKind(const Hand& hand){
+    return hand.value == 11;
+}
+
+HandRank FiveOfAKindChecker::check(const Hand& hand){
+    if (isFiveOfAKind(hand)){
+        std::cout << "Detected FIVE OF A KIND\n";
+        return HandRank::FIVE_OF_A_KIND;
+    }
+
+    if (nextChecker)
+        return nextChecker->check(hand);
+
+    return HandRank::HIGH_CARD;
+}
