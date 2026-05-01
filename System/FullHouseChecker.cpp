a5912df38e2f3170d3b64e29a531316de1785a6a
Nifaxha <harizriza30@gmail.com>
2026-05-02 00:19:22 +0700

    Implementasi algoritma logika untuk FullHouseChecker

--- System/FullHouseChecker.cpp
+++ System/FullHouseChecker.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <unordered_map>
 #include "FullHouseChecker.h"
 
-bool isFullHouse(const Hand& hand){
-    return hand.value == 7;
+bool isFullHouse(const Hand& hand) {
+    std::unordered_map<int, int> rankCounts;
+    for (const Card& card : hand.cards) rankCounts[card.rank]++;
+    bool hasThree = false;
+    int pairCount = 0;
+    
+    for (const auto& pair : rankCounts) {
+        if (pair.second >= 3) hasThree = true;
+        if (pair.second >= 2) pairCount++; 
+    }
+    // Jika ada 3-of-a-kind, otomatis pairCount akan menjadi minimal 1. 
+    // Jadi untuk full house kita butuh pairCount >= 2.
+    return hasThree && (pairCount >= 2);
 }
 
-HandRank FullHouseChecker::check(const Hand& hand){
-    if (isFullHouse(hand)){
+HandRank FullHouseChecker::check(const Hand& hand) {
+    if (isFullHouse(hand)) {
         std::cout << "Detected FULL HOUSE\n";
         return HandRank::FULL_HOUSE;
     }
-
-    if (nextChecker)
-        return nextChecker->check(hand);
-
+    if (nextChecker) return nextChecker->check(hand);
     return HandRank::HIGH_CARD;
 }
