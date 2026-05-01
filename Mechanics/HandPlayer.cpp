1e2b75c95a2b68f801388959ad9fdb63db4b4646
Yorrr-ux <yoriraha06@gmail.com>
2026-05-01 23:00:27 +0700

    Refactor HandPLayer

--- Mechanics/HandPlayer.cpp
+++ Mechanics/HandPlayer.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "HandPlayer.h"
 
-void HandPlayer::playHand(){
+ChosenHand HandPlayer::playHand(const Hand& hand) {
     std::cout << "Player selects cards to play...\n";
+    ChosenHand chosen;
+
+    // Simulasi Otomatis: Pemain mengambil maksimal 5 kartu pertama dari tangannya
+    int cardsToPlay = std::min(5, (int)hand.cards.size());
+    for (int i = 0; i < cardsToPlay; ++i) {
+        chosen.addCard(hand.cards[i]);
+        std::cout << "Selected Card -> Rank: " << hand.cards[i].rank << ", Suit: " << hand.cards[i].suit << "\n";
+    }
+
+    return chosen;
 }
