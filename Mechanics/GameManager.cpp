707930a84e8ef57e4394c1d09bfe8c936ef2de4c
Yorrr-ux <yoriraha06@gmail.com>
2026-05-12 14:09:19 +0700

    Update GameManager.cpp

--- Mechanics/GameManager.cpp
+++ Mechanics/GameManager.cpp
@@ -2,24 +2,24 @@
 #include "GameManager.h"
 
 void GameManager::runSession() {
-    std::cout << "=== Run Started ===\n";
+    std::cout << "=== Balatro Clone Started ===\n";
 
-    // 1. Generate kartu acak
+    // 1. Generate 8 kartu
     Hand generatedHand = handGenerator.generateHand();
 
-    // 2. Pemain memilih kartu dari tangannya
+    // 2. Player milih manual (maks 5)
     ChosenHand chosenHand = handPlayer.playHand(generatedHand);
 
-    // 3. Sistem mengubah ChosenHand ke Hand dasar untuk dievaluasi
+    // 3. Konversi format
     Hand handToScore = chosenHand.toHand();
 
-    // 4. Proses skoring melewati Chain of Responsibility
+    // 4. Kalkulasi skor dari chain of responsibility
     int score = scoringRule.scoreHand(handToScore);
 
-    // 5. Cek kemenangan & Hadiah
+    // 5. Cek kondisi menang & uang
     bool win = blindRule.checkBlind(score);
     int reward = rewardRule.earnMoney(win, score);
 
     std::cout << "Money gained: " << reward << "\n";
-    std::cout << "=== Run Ended ===\n";
+    std::cout << "=== Round Ended ===\n";
 }
