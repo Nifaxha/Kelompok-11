ef5acc5b7d997c118d2ce8f21f02f6fc05959608
Yorrr-ux <yoriraha06@gmail.com>
2026-06-02 18:43:17 +0700

    Update BlindState.h

--- System/Blinds/BlindState.h
+++ System/Blinds/BlindState.h
@@ -9,6 +9,10 @@ public:
     virtual std::string getName() const = 0;
     virtual int getTargetScore(int ante) const = 0;
     virtual int getRewardMoney() const = 0;
+    
+    // TAMBAHKAN BARIS INI: Wajibkan semua Blind punya Tag Skip
+    virtual std::string getSkipRewardTag() const = 0; 
+    
     virtual PendingCommand createSkipRewardCommand() const = 0;
     virtual std::unique_ptr<BlindState> nextState(int& ante) const = 0;
 };
