ce2359196e7b4b75dc707387b0f32a4f23d1b1c6
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:25:45 +0700

    Update JollyJoker.cpp

--- System/Jokers/JollyJoker.cpp
+++ System/Jokers/JollyJoker.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include "JollyJoker.h"
 
-JollyJoker::JollyJoker(IScoring* scoring) : JokerDecorator(scoring) {}
-
 ScoreContext JollyJoker::scoreHand(const Hand& hand) {
-    ScoreContext ctx = JokerDecorator::scoreHand(hand); // Tarik data chips & mult
-    
-    std::cout << "[Joker Active] Jolly Joker: Memberikan +4 Multiplier!\n";
-    ctx.mult += 4; // Hanya ubah multiplier
-    
+    ScoreContext ctx = JokerDecorator::scoreHand(hand);
+    std::cout << "[JOKER] Jolly Joker aktif: +8 Multiplier!\n";
+    ctx.mult += 8; 
     return ctx;
 }
