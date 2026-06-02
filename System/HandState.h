e4f14e084834ba2a1f655e8ed0b73410c257a2ad
Yorrr-ux <yoriraha06@gmail.com>
2026-06-02 17:00:47 +0700

    Create HandState.h

---
+++ System/HandState.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "Card.h"
+#include "Hand.h"
+#include "Deck.h"
+#include "ChosenHand.h"
+
+class HandState {
+private:
+    std::vector<Card> activeCards;
+    int maxHandSize = 8; // Batas maksimal kartu di tangan
+
+public:
+    void drawFromDeck(Deck& deck);
+    Hand getHandAsStruct() const; 
+    void removePlayedCards(const ChosenHand& chosen);
+    void clearHand();
+};
