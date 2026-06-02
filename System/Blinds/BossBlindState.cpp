aed56c7ff98dd3ee761d4ba0ee31a37ad87e91f8
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:56:53 +0700

    Update BossBlindState.cpp

--- System/Blinds/BossBlindState.cpp
+++ System/Blinds/BossBlindState.cpp
@@ -5,12 +5,12 @@ std::string BossBlindState::getName() const { return "Boss Blind"; }
 int BossBlindState::getTargetScore(int ante) const { return 600 * ante; }
 int BossBlindState::getRewardMoney() const { return 5; }
 
-// Boss Blind umumnya tidak bisa di-skip
 PendingCommand BossBlindState::createSkipRewardCommand() const {
-    return PendingCommand{ CommandTiming::Immediate, true, nullptr }; 
+    // Gunakan timing Start sesuai dokumen
+    return PendingCommand(CommandTiming::Start, true, nullptr); 
 }
 
 std::unique_ptr<BlindState> BossBlindState::nextState(int& ante) const {
-    ante++; // Boss Blind menaikkan level Ante
-    return std::make_unique<SmallBlindState>();
+    ante++; 
+    return std::unique_ptr<SmallBlindState>(new SmallBlindState());
 }
