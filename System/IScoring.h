d491812706dd41171e84c7d5f553fa126d0c5844
Yorrr-ux <yoriraha06@gmail.com>
2026-06-02 16:33:58 +0700

    Update IScoring.h

--- System/IScoring.h
+++ System/IScoring.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Hand.h"
+#include "ScoreContext.h" // Tambahkan ini
 
 class IScoring {
 public:
     virtual ~IScoring() = default;
-    virtual int scoreHand(const Hand& hand) = 0;
+    virtual ScoreContext scoreHand(const Hand& hand) = 0; // Ubah int jadi ScoreContext
 };
