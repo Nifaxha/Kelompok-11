45b3e98a2a1f370f07e96942910dcf1a1c249ce0
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:33:01 +0700

    Update JollyJoker.cpp

--- System/Jokers/JollyJoker.cpp
+++ System/Jokers/JollyJoker.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-#include "JollyJoker.h"
+#pragma once
+#include "JokerDecorator.h"
 
-ScoreContext JollyJoker::scoreHand(const Hand& hand) {
-    ScoreContext ctx = JokerDecorator::scoreHand(hand);
-    std::cout << "[JOKER] Jolly Joker aktif: +8 Multiplier!\n";
-    ctx.mult += 8; 
-    return ctx;
-}
+class JollyJoker : public JokerDecorator {
+public:
+    JollyJoker(IScoring* scoring);
+    ScoreContext scoreHand(const Hand& hand) override;
+};
