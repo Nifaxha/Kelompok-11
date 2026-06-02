04229e01e9e21655bcd51a4ff5127969a67e0fe9
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:28:38 +0700

    Create BlindState.h

---
+++ System/Blinds/BlindState.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <memory>
+#include "RewardCommand.h"
+
+class BlindState {
+public:
+    virtual ~BlindState() = default;
+    virtual std::string getName() const = 0;
+    virtual int getTargetScore(int ante) const = 0;
+    virtual int getRewardMoney() const = 0;
+    virtual PendingCommand createSkipRewardCommand() const = 0;
+    virtual std::unique_ptr<BlindState> nextState(int& ante) const = 0;
+};
