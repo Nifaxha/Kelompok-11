4862963219de6878b7b610eb7d2659761d834f80
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:32:45 +0700

    Update GreedyJoker.h

--- System/Jokers/GreedyJoker.h
+++ System/Jokers/GreedyJoker.h
@@ -1,16 +1,8 @@
-#include <iostream>
-#include "GreedyJoker.h"
+#pragma once
+#include "JokerDecorator.h"
 
-GreedyJoker::GreedyJoker(IScoring* scoring) : JokerDecorator(scoring) {}
-
-ScoreContext GreedyJoker::scoreHand(const Hand& hand) {
-    ScoreContext ctx = JokerDecorator::scoreHand(hand);
-
-    // Cek apakah chips dasar tinggi (indikasi hand bagus, misal Straight ke atas)
-    if (ctx.chips >= 30) {
-        std::cout << "[Joker Active] Greedy Joker: Kombinasi kuat! Multiplier dikalikan x2!\n";
-        ctx.mult *= 2; 
-    }
-
-    return ctx;
-}   
+class GreedyJoker : public JokerDecorator {
+public:
+    GreedyJoker(IScoring* scoring);
+    ScoreContext scoreHand(const Hand& hand) override;
+};
