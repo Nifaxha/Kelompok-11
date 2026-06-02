64bd31222274ff2166b48356d0c5e5ea7b682f00
Nifaxha <harizriza30@gmail.com>
2026-06-02 17:25:42 +0700

    Update GreedyJoker.cpp

--- System/Jokers/GreedyJoker.cpp
+++ System/Jokers/GreedyJoker.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include "GreedyJoker.h"
 
-GreedyJoker::GreedyJoker(IScoring* scoring) : JokerDecorator(scoring) {}
-
-int GreedyJoker::scoreHand(const Hand& hand) {
-    int baseScore = JokerDecorator::scoreHand(hand);
-
-    // Jika skor >= 25, artinya hand tersebut minimal adalah Straight atau lebih tinggi
-    if (baseScore >= 25) {
-        std::cout << "[Joker Active] Greedy Joker: Kombinasi kuat terdeteksi! Skor dikalikan x2!\n";
-        return baseScore * 2;
+ScoreContext GreedyJoker::scoreHand(const Hand& hand) {
+    ScoreContext ctx = JokerDecorator::scoreHand(hand);
+    // Jika chips dari kombinasi kartu >= 30 (Straight ke atas)
+    if (ctx.chips >= 30) {
+        std::cout << "[JOKER] Greedy Joker mendeteksi kombinasi kuat: Multiplier x2!\n";
+        ctx.mult *= 2; 
     }
-
-    return baseScore;
+    return ctx;
 }
