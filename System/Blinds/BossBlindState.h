48b38f7c87a5a9478b668b2d61026c3b7abcadf4
Yorrr-ux <yoriraha06@gmail.com>
2026-06-02 18:43:42 +0700

    Update BossBlindState.h

--- System/Blinds/BossBlindState.h
+++ System/Blinds/BossBlindState.h
@@ -6,6 +6,7 @@ public:
     std::string getName() const override;
     int getTargetScore(int ante) const override;
     int getRewardMoney() const override;
+    std::string getSkipRewardTag() const override; // Tambahkan ini
     PendingCommand createSkipRewardCommand() const override;
     std::unique_ptr<BlindState> nextState(int& ante) const override;
 };
